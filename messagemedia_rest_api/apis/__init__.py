5f9d208d9fe0a582f8e463d4e23502f5953c047f
Paul Colby <git@colby.id.au>
2017-08-08 11:54:46 +1000

    Point to https://developers.messagemedia.com

--- messagemedia_rest_api/apis/__init__.py
+++
@@ -1,7 +0,0 @@
-from __future__ import absolute_import
-
-# import apis into api package
-from .delivery_reports_api import DeliveryReportsApi
-from .messaging_api import MessagingApi
-from .messaging_reports_api import MessagingReportsApi
-from .replies_api import RepliesApi
